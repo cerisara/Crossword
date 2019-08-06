16783cff94ef9997edb53b944e9ecead5b1056b6
CC <cerisara@loria.fr>
2019-08-06 07:06:56 +0200

    my game

--- js/script.js
+++ js/script.js
@@ -6,149 +6,157 @@
 		// two entries: an across entry and a down entry
 		var puzzleData = [
 			 	{
-					clue: "First letter of greek alphabet",
-					answer: "alpha",
+					clue: "Réseau local",
+					answer: "ethernet",
 					position: 1,
 					orientation: "across",
 					startx: 1,
 					starty: 1
 				},
 			 	{
-					clue: "Not a one ___ motor, but a three ___ motor",
-					answer: "phase",
-					position: 3,
-					orientation: "across",
-					startx: 7,
-					starty: 1
-				},
-				{
-					clue: "Created from a separation of charge",
-					answer: "capacitance",
-					position: 5,
-					orientation: "across",
-					startx: 1,
-					starty: 3
-				},
-				{
-					clue: "The speeds of engines without and accelaration",
-					answer: "idlespeeds",
-					position: 8,
-					orientation: "across",
-					startx: 1,
-					starty: 5
-				},
-				{
-					clue: "Complex resistances",
-					answer: "impedances",
-					position: 10,
-					orientation: "across",	
-					startx: 2,
-					starty: 7
-				},
-				{
-					clue: "This device is used to step-up, step-down, and/or isolate",
-					answer: "transformer",
-					position: 13,
-					orientation: "across",
-					startx: 1,
-					starty: 9
-				},
-				{
-					clue: "Type of ray emitted frm the sun",
-					answer: "gamma",
-					position: 16,
-					orientation: "across",
-					startx: 1,
-					starty: 11
-				},
-				{
-					clue: "C programming language operator",
-					answer: "cysan",
-					position: 17,
-					orientation: "across",
-					startx: 7,
-					starty: 11
-				},
-				{
-					clue: "Defines the alpha-numeric characters that are typically associated with text used in programming",
-					answer: "ascii",
+					clue: "Société célèbre",
+					answer: "ea",
 					position: 1,
 					orientation: "down",
 					startx: 1,
 					starty: 1
 				},
-				{
-					clue: "Generally, if you go over 1kV per cm this happens",
-					answer: "arc",
+			 	{
+					clue: "Taux de transmission",
+					answer: "tx",
 					position: 2,
 					orientation: "down",
-					startx: 5,
+					startx: 2,
 					starty: 1
 				},
-				{
-					clue: "Control system strategy that tries to replicate the human through process (abbr.)",
-					answer: "ann",
+			 	{
+					clue: "clients de FAI",
+					answer: "heberges",
+					position: 3,
+					orientation: "down",
+					startx: 3,
+					starty: 1
+				},
+			 	{
+					clue: "pas applicable",
+					answer: "na",
 					position: 4,
 					orientation: "down",
-					startx: 9,
+					startx: 6,
 					starty: 1
 				},
-				{
-					clue: "Greek variable that usually describes rotor positon",
-					answer: "theta",
+			 	{
+					clue: "standard base sur XML",
+					answer: "tei",
+					position: 5,
+					orientation: "down",
+					startx: 8,
+					starty: 1
+				},
+			 	{
+					clue: "Au centre",
+					answer: "axe",
 					position: 6,
+					orientation: "across",
+					startx: 1,
+					starty: 2
+				},
+			 	{
+					clue: "Unité de base",
+					answer: "bit",
+					position: 7,
+					orientation: "across",
+					startx: 3,
+					starty: 3
+				},
+			 	{
+					clue: "algorithme de base",
+					answer: "tri",
+					position: 8,
+					orientation: "down",
+					startx: 5,
+					starty: 3
+				},
+			 	{
+					clue: "Nombre",
+					answer: "pi",
+					position: 9,
+					orientation: "across",
+					startx: 7,
+					starty: 3
+				},
+			 	{
+					clue: "langage",
+					answer: "python",
+					position: 9,
 					orientation: "down",
 					startx: 7,
 					starty: 3
 				},
-				{
-					clue: "Electromagnetic (abbr.)",
-					answer: "em",
-					position: 7,
+			 	{
+					clue: "documents HTML",
+					answer: "pages",
+					position: 10,
 					orientation: "down",
-					startx: 11,
-					starty: 3
+					startx: 1,
+					starty: 4
 				},
-				{
-					clue: "No. 13 across does this to a voltage",
-					answer: "steps",
-					position: 9,
-					orientation: "down",
-					startx: 5,
-					starty: 5
-				},
-				{
-					clue: "Emits a lout wailing sound",
-					answer: "siren",
+			 	{
+					clue: "Tracing",
+					answer: "ray",
 					position: 11,
+					orientation: "across",
+					startx: 5,
+					starty: 4
+				},
+			 	{
+					clue: "Unité de mesure",
+					answer: "giga",
+					position: 12,
+					orientation: "across",
+					startx: 1,
+					starty: 6
+				},
+			 	{
+					clue: "Interjection",
+					answer: "ah",
+					position: 13,
+					orientation: "across",
+					startx: 6,
+					starty: 6
+				},
+			 	{
+					clue: "langage",
+					answer: "ada",
+					position: 13,
 					orientation: "down",
-					startx: 11,
+					startx: 6,
+					starty: 6
+				},
+			 	{
+					clue: "Système d'exploitation",
+					answer: "dos",
+					position: 14,
+					orientation: "across",
+					startx: 6,
 					starty: 7
 				},
-				{
-					clue: "Information technology (abbr.)",
-					answer: "it",
-					position: 12,
-					orientation: "down",
+			 	{
+					clue: "Appel au service après-vente",
+					answer: "sos",
+					position: 15,
+					orientation: "across",
 					startx: 1,
 					starty: 8
 				},
-				{
-					clue: "Asynchronous transfer mode (abbr.)",
-					answer: "atm",
-					position: 14,
-					orientation: "down",
-					startx: 3,
-					starty: 9
+			 	{
+					clue: "Ce n'est pas un nombre",
+					answer: "nan",
+					position: 16,
+					orientation: "across",
+					startx: 5,
+					starty: 8
 				},
-				{
-					clue: "Offset current control (abbr.)",
-					answer: "occ",
-					position: 15,
-					orientation: "down",
-					startx: 7,
-					starty: 9
-				}
 			] 
 	
 		$('#puzzle-wrapper').crossword(puzzleData);
